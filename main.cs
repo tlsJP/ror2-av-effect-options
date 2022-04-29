ae5c5c7acb06703c25a731a5de6f1a4aecd92971
Lexxy Fox <lexxyfox@gmail.com>
2022-04-29 15:45:20 -0400

    Optimized

--- main.cs
+++ main.cs
@@ -1,5 +1,3 @@
-namespace ᛪᚶᚬᛣᚿᛐᚯᚰᛛᚸᚡᚢᛯᛆᛒᛡ { // C# 10.0 supports file-wide namespaces, but currently only C# 8.0 is supported.
-
 [BepInEx.BepInPlugin("XmdzoIwkDXCTKH64", "AV Effect Options", "1.0.0")]
 [BepInEx.BepInDependency("com.rune580.riskofoptions", BepInEx.BepInDependency.DependencyFlags.SoftDependency)]
 public sealed class ᚠᛯᛇᛁᚢᛮᛶᛁᛋᚴᛪᛩᚣᛉᚠᛉ: BepInEx.BaseUnityPlugin {
@@ -10,7 +8,6 @@ public sealed class ᚠᛯᛇᛁᚢᛮᛶᛁᛋᚴᛪᛩᚣᛉᚠᛉ: BepInEx.Ba
   private BepInEx.Configuration.ConfigEntry<bool> enableStickyBomb;
   private BepInEx.Configuration.ConfigEntry<bool> enableMoltenPerforator;
   
-  
   public void Awake() {
     enableStickyBomb = Config.Bind("Item Effects", "Remove Sticky Bomb", true, "Enables the Sticky Bomb Visual");
 		enableMoltenPerforator = Config.Bind("Item Effects", "Remove Molten Perforator", true, "Enables Molten Perforator Visuals");
@@ -18,30 +15,19 @@ public sealed class ᚠᛯᛇᛁᚢᛮᛶᛁᛋᚴᛪᛩᚣᛉᚠᛉ: BepInEx.Ba
 		StickyBombPrefab.SetActive(enableStickyBomb.Value);
 		MoltenPerforatorPrefab.SetActive(enableMoltenPerforator.Value);
 		
-		System.Console.WriteLine("Hewwos!!");
-		
-    enableStickyBomb.SettingChanged += (sender, args) => {
-      System.Console.WriteLine("Sticky Bomb Changed!");
-      System.Console.WriteLine(sender);
-      System.Console.WriteLine(args);
-      StickyBombPrefab.SetActive(enableStickyBomb.Value);
-    };
-    enableMoltenPerforator.SettingChanged += (sender, args) => { 
-      System.Console.WriteLine("Molten Perferator Changed!");
-      System.Console.WriteLine(sender);
-      System.Console.WriteLine(args);
-      MoltenPerforatorPrefab.SetActive(enableMoltenPerforator.Value);
-    };
+    enableStickyBomb.SettingChanged += (x, _) => 
+      StickyBombPrefab.SetActive(((BepInEx.Configuration.ConfigEntry<bool>) x).Value);
+    enableMoltenPerforator.SettingChanged += (x, _) =>
+      MoltenPerforatorPrefab.SetActive(((BepInEx.Configuration.ConfigEntry<bool>) x).Value);
 		
     if (BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("com.rune580.riskofoptions"))
       ᚻᛀᛧᚠᚾᛢᚻᛐᛉᚼᛤᛪᚲᛇᛎᛛ();
   }
   
+  [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.NoInlining)]
   private void ᚻᛀᛧᚠᚾᛢᚻᛐᛉᚼᛤᛪᚲᛇᛎᛛ() {
     RiskOfOptions.ModSettingsManager.AddOption(new RiskOfOptions.Options.CheckBoxOption(enableStickyBomb));
 		RiskOfOptions.ModSettingsManager.AddOption(new RiskOfOptions.Options.CheckBoxOption(enableMoltenPerforator));
   }
-
-}
 
 }
