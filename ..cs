143d96847cbf793d305d5a6517854bd7e428753b
Lexxy Fox <lexxyfox@gmail.com>
2022-05-01 18:53:57 -0400

    Added icon to ROO menu.

--- ..cs
+++ ..cs
@@ -3,6 +3,8 @@ using BepInEx.Bootstrap;
 using BepInEx.Configuration;
 using RiskOfOptions;
 using RiskOfOptions.Options;
+using System.IO;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
@@ -18,21 +20,37 @@ public sealed class _: BaseUnityPlugin {
     ᚭᛣᛮᛨᚶᛟᚷᚴ("FireballsOnHit/FireMeatBallGhost", "Enable Molten Perforator", "Enables the Molten Perforator visuals");
     ᚭᛣᛮᛨᚶᛟᚷᚴ("Tonic/TonicBuffEffect", "Enable Spinel Tonic", "Enables the Spinel Tonic screen effect");
     ᚭᛣᛮᛨᚶᛟᚷᚴ("KillEliteFrenzy/NoCooldownEffect", "Enable Brainstalks", "Enables the Brainstalks screen effect. Note: re-enabling may not take effect until next stage.");
+    if (ᛢᛪᛔᚸᚽᚹᛃᚬ) ᚧᛃᛍᛣᛩᚱᚦᛕ();
   }
   
   [MethodImpl(768)]
   private void ᚭᛣᛮᛨᚶᛟᚷᚴ(string ᚱᛢᛊᚢᚴᛤᛉᚣ, string ᛆᛇᚶᛔᛒᛄᚤᛪ, string ᚡᛧᛞᛍᛏᛀᛕᚵ, string ᚩᛶᛠᚿᚵᚯᚹᛴ = "Item Effects") {
-    var prefab = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/" + ᚱᛢᛊᚢᚴᛤᛉᚣ + ".prefab").WaitForCompletion();
+    var prefab = Addressables.LoadAsset<GameObject>("RoR2/Base/" + ᚱᛢᛊᚢᚴᛤᛉᚣ + ".prefab").WaitForCompletion();
     var config = Config.Bind(ᚩᛶᛠᚿᚵᚯᚹᛴ, ᛆᛇᚶᛔᛒᛄᚤᛪ, true, ᚡᛧᛞᛍᛏᛀᛕᚵ);
     // todo: should the following class be merged into this one?
     config.SettingChanged += (x, _) =>
       prefab.SetActive(((ConfigEntry<bool>) x).Value);
     prefab.SetActive(config.Value);
-    if (ᛢᛪᛔᚸᚽᚹᛃᚬ)
-      ᚾᛏᛧᛨᚭᛋᛳᚩ(config);
+    if (ᛢᛪᛔᚸᚽᚹᛃᚬ) ᚾᛏᛧᛨᚭᛋᛳᚩ(config);
   }
   
   [MethodImpl(520)]
   private void ᚾᛏᛧᛨᚭᛋᛳᚩ(ConfigEntry<bool> _) =>
     ModSettingsManager.AddOption(new CheckBoxOption(_));
+  
+  [MethodImpl(520)]
+  private void ᚧᛃᛍᛣᛩᚱᚦᛕ() {
+    using var stream = GetType().Assembly.GetManifestResourceStream(".");
+    var texture = new Texture2D(0, 0);
+    var imgdata = new byte[stream.Length];
+    stream.Read(imgdata, 0, imgdata.Length);
+    if (ImageConversion.LoadImage(texture, imgdata))
+      ModSettingsManager.SetModIcon(
+        Sprite.Create(
+          texture,
+          new Rect(0, 0, texture.width, texture.height),
+          new Vector2(0, 0)
+        )
+      );
+  }
 }
