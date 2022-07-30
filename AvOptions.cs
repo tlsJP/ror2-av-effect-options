c83c87618d8a5b077b0aa71f73cf89e0f086df15
JP <jpalbert81@gmail.com>
2022-07-30 15:17:58 -0700

    change to LF only

--- AvOptions.cs
+++ AvOptions.cs
@@ -45,6 +45,7 @@ public sealed class AvOptions : BaseUnityPlugin
     //private static LoopSoundPlayer PlimpAudio;
     //private static LoopSoundDef PlimpFlightSoundLoop;
     //private static string PlimpFlightSoundSoundName;
+
     private static ProjectileController PlimpController;
 
 
