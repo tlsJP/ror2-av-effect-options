4ee67b20d3da1ea6d36c6692d4f09c90cfcab532
JP <jpalbert81@gmail.com>
2022-07-31 16:23:28 -0700

    fix imports

---
+++ src/PlasmaShrimpConfiguration.cs
@@ -0,0 +1,27 @@
+﻿
+using BepInEx.Configuration;
+using RoR2;
+using System;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace com.thejpaproject.avoptions
+{
+    class PlasmaShrimpConfiguration : AvConfiguration
+    {
+
+        private static EffectComponent MissileVoidOrbEffect;
+
+        public PlasmaShrimpConfiguration(ConfigEntry<bool> configEntry) : base(configEntry) { }
+
+        private protected override void SetBehavior()
+        {
+            var missileVoidOrbEffectPrefab = Addressables.LoadAsset<GameObject>("RoR2/DLC1/MissileVoid/MissileVoidOrbEffect.prefab").WaitForCompletion();
+            MissileVoidOrbEffect = missileVoidOrbEffectPrefab.GetComponent<EffectComponent>();
+            MissileVoidOrbEffect.enabled = ConfigEntry.Value;
+            MissileVoidOrbEffect.soundName = ConfigEntry.Value ? "Play_item_void_critGlasses" : "";
+        }
+
+        private protected override void HandleEvent(object x, EventArgs args) => MissileVoidOrbEffect.enabled = ((ConfigEntry<bool>)x).Value;
+    }
+}
