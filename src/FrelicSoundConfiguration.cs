714dceb08a790936d454e27f2ea1ab0bc8515396
JP <jpalbert81@gmail.com>
2022-08-02 15:43:28 -0700

    ??

---
+++ src/FrelicSoundConfiguration.cs
@@ -0,0 +1,23 @@
+﻿
+using BepInEx.Configuration;
+using System;
+
+namespace com.thejpaproject.avoptions
+{
+    internal class FrelicSoundConfiguration : AvConfiguration
+    {
+        public FrelicSoundConfiguration(ConfigFile configFile) :
+            base(configFile, "Item Effects", "Enable Frost Relic Sound", "Enables the sound effects of Frost Relic's on-kill proc.")
+        { }
+
+        private protected override void HandleEvent(object x, EventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        private protected override void SetBehavior()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
