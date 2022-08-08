86e20f113ab0b4038c2dd533aab98d9893919d04
JP <jpalbert81@gmail.com>
2022-08-08 14:31:01 -0700

    update blast shower

--- avoptions/configurations/BlastShowerConfiguration.cs
+++ avoptions/configurations/BlastShowerConfiguration.cs
@@ -9,28 +9,31 @@ namespace com.thejpaproject.avoptions.configurations
     {
         private static Transform CleanseTransform;
 
+        private static readonly String DESCRIPTION =
+@"Enables Blast Shower's effects.
+
+Disable: Effective immediately
+Enable: Effective on next level
+";
+
         public BlastShowerConfiguration(ConfigFile configFile) :
-            base(configFile, "Item Effects", "Enable Blast Shower", "Enables Blast Shower's effects.", true)
+            base(configFile, "Item Effects", "Enable Blast Shower", DESCRIPTION)
         { }
 
-        private protected override void HandleEvent(object x, EventArgs args)
-        {
-            var y = ((ConfigEntry<bool>)x).Value;
-            for (var i = 0; i < CleanseTransform.childCount; i++)
-                CleanseTransform.GetChild(i).gameObject.SetActive(y);
-        }
+        private protected override void HandleEvent(object x, EventArgs args) => SetChildrenStatus(((ConfigEntry<bool>)x).Value);
 
         private protected override void SetBehavior()
         {
 
             var prefab = Addressables.LoadAsset<GameObject>("RoR2/Base/Cleanse/CleanseEffect.prefab").WaitForCompletion();
             CleanseTransform = prefab.transform;
+            SetChildrenStatus(ConfigEntry.Value);
+        }
 
-            if (!ConfigEntry.Value)
-            {
-                for (var i = 0; i < CleanseTransform.childCount; i++)
-                    CleanseTransform.GetChild(i).gameObject.SetActive(false);
-            }
+        private void SetChildrenStatus(bool active)
+        {
+            for (var i = 0; i < CleanseTransform.childCount; i++)
+                CleanseTransform.GetChild(i).gameObject.SetActive(active);
         }
     }
 }
