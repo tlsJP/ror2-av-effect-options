9dc846cf209a13f24630b7d220025d0bf06f1c80
JP <jpalbert81@gmail.com>
2022-08-25 16:26:30 -0700

    Stub viend sfx

---
+++ avoptions/configurations/ViendAudioConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx.Configuration;
+using System;
+
+namespace com.thejpaproject.avoptions.configurations
+{
+    internal class ViendAudioConfiguration : AvConfiguration
+    {
+        public ViendAudioConfiguration(ConfigFile configFile, string category, string key, string description, bool defaultSetting = true) : 
+            base(configFile, "SOTV Item Effects", "Enable Void Fiend SFX", "Enable run/jump sound effects", defaultSetting)
+        {}
+
+        private protected override void HandleEvent(object x, EventArgs args)
+        {
+            // TODO
+        }
+
+        private protected override void SetBehavior()
+        {
+            // TODO
+        }
+    }
+}
