86f447ac00368449b2f2e20eb4b23028b9b3b881
JP <jpalbert81@gmail.com>
2022-08-25 18:38:59 -0700

    viend sfx toggle complete

--- avoptions/configurations/ViendAudioConfiguration.cs
+++ avoptions/configurations/ViendAudioConfiguration.cs
@@ -1,22 +1,33 @@
 ﻿using BepInEx.Configuration;
 using System;
+using RoR2;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 namespace com.thejpaproject.avoptions.configurations
 {
     internal class ViendAudioConfiguration : AvConfiguration
     {
-        public ViendAudioConfiguration(ConfigFile configFile, string category, string key, string description, bool defaultSetting = true) : 
-            base(configFile, "SOTV Item Effects", "Enable Void Fiend SFX", "Enable run/jump sound effects", defaultSetting)
+
+        private SprintEffectController _sprintEffectController;
+
+        public ViendAudioConfiguration(ConfigFile configFile) : 
+            base(configFile, "SOTV Item Effects", "Enable Viend Sprint/Jump SFX", "Enable run/jump sound effects", true)
         {}
 
         private protected override void HandleEvent(object x, EventArgs args)
         {
-            // TODO
+            var enabled = ((ConfigEntry<bool>)x).Value;
+
+            _sprintEffectController.enabled = enabled;
         }
 
         private protected override void SetBehavior()
         {
-            // TODO
+            var voidManPrefab = Addressables.LoadAsset<GameObject>("RoR2/DLC1/VoidSurvivor/VoidSurvivorBody.prefab").WaitForCompletion();
+            _sprintEffectController = voidManPrefab.GetComponent<SprintEffectController>();
+
+            
         }
     }
 }
