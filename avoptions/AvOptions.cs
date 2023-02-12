9b466f5d04d1b9558f6575c3fa33411ca8ccac70
John Albert <jpalbert81@gmail.com>
2023-02-11 17:33:53 -0800

    rename bortal vfx class

--- avoptions/AvOptions.cs
+++ avoptions/AvOptions.cs
@@ -4,84 +4,84 @@ using System.Runtime.CompilerServices;
 
 namespace com.thejpaproject.avoptions
 {
-  [BepInPlugin("com.thejpaproject.AVFX_Options", "JP's AV Effect Options", "2.1.0")]
-  [BepInDependency("com.rune580.riskofoptions", (BepInDependency.DependencyFlags)2)]
-  public sealed class AvOptions : BaseUnityPlugin
-  {
-
-    private PrefabConfigurer _prefabConfigurer;
-
-    private BlastShowerConfiguration _blastShowerConfiguration;
-    private FireworkTailConfiguration _fireworkTailConfiguration;
-    private FrelicAvConfiguration _frelicAvConfiguration;
-    private IdpVisualConfiguration _idpVisualConfiguration;
-    private KjaroVisualConfiguration _kjaroVisualConfiguration;
-    private PlasmaShrimpConfiguration _plasmaShrimpConfiguration;
-    private RoseBucklerConfiguration _roseBucklerConfiguration;
-    private RunaldsVisualConfiguration _runaldsVisualConfiguration;
-    private ViendAudioConfiguration _viendAudioConfiguration;
-    private WungusVisualConfiguration _wungusVisualConfiguration;
-    private WungusAudioConfiguration _wungusAudioConfiguration;
-    private BortalConfiguration _bortalConfiguration;
-    private GortalConfiguration _gortalConfiguration;
-
-    [MethodImpl(768)]
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "RoR2 Mod Lifecycle Method")]
-    private void Awake()
+    [BepInPlugin("com.thejpaproject.AVFX_Options", "JP's AV Effect Options", "2.1.0")]
+    [BepInDependency("com.rune580.riskofoptions", (BepInDependency.DependencyFlags)2)]
+    public sealed class AvOptions : BaseUnityPlugin
     {
 
-      _prefabConfigurer = PrefabConfigurer.GetInstance(Config);
+        private PrefabConfigurer _prefabConfigurer;
 
-      try
-      {
+        private BlastShowerConfiguration _blastShowerConfiguration;
+        private FireworkTailConfiguration _fireworkTailConfiguration;
+        private FrelicAvConfiguration _frelicAvConfiguration;
+        private IdpVisualConfiguration _idpVisualConfiguration;
+        private KjaroVisualConfiguration _kjaroVisualConfiguration;
+        private PlasmaShrimpConfiguration _plasmaShrimpConfiguration;
+        private RoseBucklerConfiguration _roseBucklerConfiguration;
+        private RunaldsVisualConfiguration _runaldsVisualConfiguration;
+        private ViendAudioConfiguration _viendAudioConfiguration;
+        private WungusVisualConfiguration _wungusVisualConfiguration;
+        private WungusAudioConfiguration _wungusAudioConfiguration;
+        private BortalVfxConfiguration _bortalConfiguration;
+        private GortalConfiguration _gortalConfiguration;
 
-        _blastShowerConfiguration = new BlastShowerConfiguration(Config);
-        _bortalConfiguration = new BortalConfiguration(Config);
-        _prefabConfigurer.BindVfx("KillEliteFrenzy/NoCooldownEffect", "Brainstalks", "Enables Brainstalks' screen effect. Note: re-enabling may not take effect until next stage.");
+        [MethodImpl(768)]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "RoR2 Mod Lifecycle Method")]
+        private void Awake()
+        {
 
-        _fireworkTailConfiguration = new FireworkTailConfiguration(Config);
-        _frelicAvConfiguration = new FrelicAvConfiguration(Config);
+            _prefabConfigurer = PrefabConfigurer.GetInstance(Config);
 
-        _prefabConfigurer.BindVfx("IgniteOnKill/IgniteExplosionVFX", "Gasoline", "Enables Gasoline's explosion");
+            try
+            {
 
-        _gortalConfiguration = new GortalConfiguration(Config);
+                _blastShowerConfiguration = new BlastShowerConfiguration(Config);
+                _bortalConfiguration = new BortalVfxConfiguration(Config);
+                _prefabConfigurer.BindVfx("KillEliteFrenzy/NoCooldownEffect", "Brainstalks", "Enables Brainstalks' screen effect. Note: re-enabling may not take effect until next stage.");
 
-        _idpVisualConfiguration = new IdpVisualConfiguration(Config);
+                _fireworkTailConfiguration = new FireworkTailConfiguration(Config);
+                _frelicAvConfiguration = new FrelicAvConfiguration(Config);
 
-        _kjaroVisualConfiguration = new KjaroVisualConfiguration(Config);
+                _prefabConfigurer.BindVfx("IgniteOnKill/IgniteExplosionVFX", "Gasoline", "Enables Gasoline's explosion");
 
-        _prefabConfigurer.BindVfx("FireballsOnHit/FireMeatBallGhost", "Molten Perforator", "Enables the Molten Perforator visuals");
+                _gortalConfiguration = new GortalConfiguration(Config);
 
-        _plasmaShrimpConfiguration = new PlasmaShrimpConfiguration(Config);
-        _prefabConfigurer.BindVoidVfx("MissileVoid/MissileVoidOrbEffect", "Plasma Shrimp Orbs", "It's the missiles that shoot out");
+                _idpVisualConfiguration = new IdpVisualConfiguration(Config);
 
-        _roseBucklerConfiguration = new RoseBucklerConfiguration(Config);
-        _runaldsVisualConfiguration = new RunaldsVisualConfiguration(Config);
+                _kjaroVisualConfiguration = new KjaroVisualConfiguration(Config);
 
-        _prefabConfigurer.BindVfx("BleedOnHitAndExplode/BleedOnHitAndExplode_Explosion", "Shatterspleen", "Enables Shatterspleen's explosion");
-        _prefabConfigurer.BindVfx("Tonic/TonicBuffEffect", "Spinel Tonic", "Enables Spinel Tonic's screen effect");
-        _prefabConfigurer.BindVfx("StickyBomb/StickyBombGhost", "Sticky Bomb Drops", "Enables Sticky Bomb's drops");
-        _prefabConfigurer.BindVfx("StickyBomb/BehemothVFX", "Sticky Bomb Explosion", "Enables Sticky Bomb's explosion");
+                _prefabConfigurer.BindVfx("FireballsOnHit/FireMeatBallGhost", "Molten Perforator", "Enables the Molten Perforator visuals");
 
-        _prefabConfigurer.BindAsset(Category.UNIT_EFFECTS, "Base/Titan/TitanDeathEffect", "Titan Death", "Enables Stone Titan's on-death explosion. Disabling will cause Stone Titans to disappear on death instead of creating a corpse.");
+                _plasmaShrimpConfiguration = new PlasmaShrimpConfiguration(Config);
+                _prefabConfigurer.BindVoidVfx("MissileVoid/MissileVoidOrbEffect", "Plasma Shrimp Orbs", "It's the missiles that shoot out");
 
-        _viendAudioConfiguration = new ViendAudioConfiguration(Config);
+                _roseBucklerConfiguration = new RoseBucklerConfiguration(Config);
+                _runaldsVisualConfiguration = new RunaldsVisualConfiguration(Config);
 
-        _prefabConfigurer.BindAsset(Category.UNIT_EFFECTS, "Base/Vagrant/VagrantDeathExplosion", "Wandering Vagrant Death", "Enables Wandering Vagrant's on-death explosion. Disabling will cause Wandering Vagrants to disappear on death instead of creating a corpse.");
-        _prefabConfigurer.BindVfx("ExplodeOnDeath/WilloWispExplosion", "Will-o-the-Wisp", "Enables Will o' the Wisp's explosion");
-        _wungusVisualConfiguration = new WungusVisualConfiguration(Config);
-        _wungusAudioConfiguration = new WungusAudioConfiguration(Config);
+                _prefabConfigurer.BindVfx("BleedOnHitAndExplode/BleedOnHitAndExplode_Explosion", "Shatterspleen", "Enables Shatterspleen's explosion");
+                _prefabConfigurer.BindVfx("Tonic/TonicBuffEffect", "Spinel Tonic", "Enables Spinel Tonic's screen effect");
+                _prefabConfigurer.BindVfx("StickyBomb/StickyBombGhost", "Sticky Bomb Drops", "Enables Sticky Bomb's drops");
+                _prefabConfigurer.BindVfx("StickyBomb/BehemothVFX", "Sticky Bomb Explosion", "Enables Sticky Bomb's explosion");
 
-      }
-      catch (ConfigurationException e)
-      {
-        Logger.LogError(e);
-        Logger.LogError($"Failed to register configuration for {e.Message}\r{e.StackTrace}");
-      }
+                _prefabConfigurer.BindAsset(Category.UNIT_EFFECTS, "Base/Titan/TitanDeathEffect", "Titan Death", "Enables Stone Titan's on-death explosion. Disabling will cause Stone Titans to disappear on death instead of creating a corpse.");
 
-      Logger.LogInfo("Finished!");
+                _viendAudioConfiguration = new ViendAudioConfiguration(Config);
+
+                _prefabConfigurer.BindAsset(Category.UNIT_EFFECTS, "Base/Vagrant/VagrantDeathExplosion", "Wandering Vagrant Death", "Enables Wandering Vagrant's on-death explosion. Disabling will cause Wandering Vagrants to disappear on death instead of creating a corpse.");
+                _prefabConfigurer.BindVfx("ExplodeOnDeath/WilloWispExplosion", "Will-o-the-Wisp", "Enables Will o' the Wisp's explosion");
+                _wungusVisualConfiguration = new WungusVisualConfiguration(Config);
+                _wungusAudioConfiguration = new WungusAudioConfiguration(Config);
+
+            }
+            catch (ConfigurationException e)
+            {
+                Logger.LogError(e);
+                Logger.LogError($"Failed to register configuration for {e.Message}\r{e.StackTrace}");
+            }
+
+            Logger.LogInfo("Finished!");
+
+        }
 
     }
-
-  }
 }
---
+++ avoptions/configurations/BortalVfxConfiguration.cs
@@ -0,0 +1,76 @@
+﻿
+using BepInEx.Configuration;
+using System;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.Rendering.PostProcessing;
+
+namespace com.thejpaproject.avoptions.configurations
+{
+    internal class BortalVfxConfiguration : AvConfiguration
+    {
+
+        private Light _light;
+        private PostProcessVolume _postProcessVolume;
+
+        public BortalVfxConfiguration(ConfigFile configFile) :
+            base(configFile, Category.BASE_VFX, "Blue Portal Glow", "Bortal is too bright.\n\nRequires next stage to take effect")
+        { }
+
+        private protected override void HandleEvent(object x, EventArgs args)
+        {
+            if (_light != null && _postProcessVolume != null)
+            {
+                var enabled = ((ConfigEntry<bool>)x).Value;
+                _light.enabled = enabled;
+                _postProcessVolume.enabled = enabled;
+            }
+        }
+
+        private protected override void SetBehavior()
+        {
+            // Using a callback here because for some reason, the portal shop prefab won't load on construction of this class.
+            On.RoR2.UI.HUD.Awake += callback;
+            On.RoR2.Stage.Start += stageStart;
+        }
+
+        /*
+          This exists solely to capture the bortal inside the lunar shop, because the bortal in there doesn't seem to respond
+          to the callback function below
+        */
+        private void stageStart(On.RoR2.Stage.orig_Start orig, RoR2.Stage self)
+        {
+            orig(self);
+            var bortal = GameObject.Find("PortalShop");
+            if (bortal == null)
+            {
+                return;
+            }
+
+            var light = bortal.GetComponentInChildren<Light>();
+            setLightRange(light);
+        }
+
+        /*
+          The biggest offender, especially on rallypoint delta is the PostProcessVolume.
+          Simply disabling this effect is arguably enough, but lowering the light's range
+          helps as well
+        */
+        private void callback(On.RoR2.UI.HUD.orig_Awake orig, RoR2.UI.HUD self)
+        {
+            var prefab = Addressables.LoadAsset<GameObject>("RoR2/Base/PortalShop/PortalShop.prefab");
+            var gameObject = prefab.WaitForCompletion();
+
+            _light = gameObject.GetComponentInChildren<Light>();
+            setLightRange(_light);
+
+            _postProcessVolume = gameObject.GetComponentInChildren<PostProcessVolume>();
+            _postProcessVolume.enabled = _configEntry.Value;
+
+            orig(self);
+        }
+
+        private void setLightRange(Light light) => light.range = _configEntry.Value ? 100 : 20;
+
+    }
+}
